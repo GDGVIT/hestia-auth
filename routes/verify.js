f58b5cb4dadde3e5e25c9f5f50edddeffba2f67b
mdhishaamakhtar <hishaamakhtar2001.mha@gmail.com>
2020-03-26 19:47:27 +0530

    minor update

--- routes/verify.js
+++ routes/verify.js
@@ -2,17 +2,17 @@ const router = require("express").Router();
 const jwt = require("jsonwebtoken");
 router.post("/", (req, res) => {
     const token = req.body.Token;
+    const email = req.body.email;
     if (!token) {
-        return res.status(401).send(false);
+        return res.status(401).json({valid:false});
     }
     jwt.verify(token,process.env.TOKEN_SECRET,(error,data)=>{
-        if(!error){
-            res.send(true);
+        if(error){
+            return res.status(400).json({valid:false});
         } else{
-            res.status(400).send(false);
+            res.json({valid:true,userID:data._id});
         }
     })
 });
-
 
 module.exports = router;
