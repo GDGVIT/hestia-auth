6cfc6996807f9c95557645d5f30b5a485a43da8d
mdhishaamakhtar <hishaamakhtar2001.mha@gmail.com>
2020-03-29 23:21:10 +0530

    Update

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,4 @@
+const request = require('request');
 const router = require('express').Router();
 const User = require('../model/User');
 const {Bcrypt} = require('bcrypt-rust-wasm');
@@ -9,7 +10,7 @@ const Verified = require('../model/Verified');
 const cryptoRandomString = require('crypto-random-string');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const {compiledFunctionEmail} = require("../compiledPug");
-router.post("/register", async (req, res) => {
+router.post("/registerApp", async (req, res) => {
     const {error} = registerValidation(req.body);
     if (error) {
         return res.status(400).json({"Error": error.details[0].message});
@@ -50,6 +51,72 @@ router.post("/register", async (req, res) => {
     } catch (err) {
         return res.status(400).json(err);
     }
+});
+
+router.post("/register", async (req, res) => {
+    let token = req.header("g-recaptcha-response");
+    if (!token) {
+        return res.json({ responseCode: 1, responseDesc: "Please select captcha" });
+    }
+    const {error} = registerValidation(req.body);
+    if (error) {
+        return res.status(400).json({"Error": error.details[0].message});
+    }
+    const secretKey = process.env.reCAPTCHA_KEY;
+    const verificationUrl =
+        "https://www.google.com/recaptcha/api/siteverify?secret=" +
+        secretKey +
+        "&response=" +
+        token +
+        "&remoteip=" +
+        req.connection.remoteAddress;
+    request.post(verificationUrl, async(_error, _response, body) => {
+        body = JSON.parse(body);
+        if (body.success !== undefined && !body.success) {
+            return res.json({
+                responseCode: 1,
+                responseDesc: "Failed captcha verification"
+            });
+        }
+        else{
+            try {
+                const {name, email, phone, password} = req.body;
+                const emailExists = await User.findOne({where: {email}});
+                if (emailExists !== null) {
+                    return res.status(400).json({Error: "There is already an account with this email!"});
+                }
+                await User.create({
+                    name: name,
+                    email: email,
+                    phone: phone,
+                    password: bcrypt.hashSync(password)
+                });
+                const token = cryptoRandomString({length: 200, type: 'url-safe'});
+                await Verified.create({
+                    email: email,
+                    token: token
+                });
+                const text = "https://" + req.hostname + "/api/user/verifyEmail/" + token;
+                const emailTemplate = compiledFunctionEmail({
+                    name: name,
+                    link: text
+                });
+                const msg = {
+                    to: email,
+                    from: 'dscvitvellore@gmail.com',
+                    subject: 'Email Verification',
+                    html: emailTemplate
+                };
+                await sgMail.send(msg);
+                res.status(202).json({
+                    "Status": "New user added",
+                    "Verify": "Email sent for verification"
+                });
+            } catch (err) {
+                return res.status(400).json(err);
+            }
+        }
+    });
 });
 
 router.post("/login", async (req, res) => {
