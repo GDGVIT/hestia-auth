c88c66a5a0866f3aa9bca5c1658e3af967f8ad00
mdhishaamakhtar <hishaamakhtar2001.mha@gmail.com>
2020-03-29 18:36:09 +0530

    Update

--- compiledPug.js
+++ compiledPug.js
@@ -2,6 +2,8 @@ const pug = require("pug");
 
 const compiledFunctionEmail = pug.compileFile('email.pug');
 const compiledFunctionForgotPassword = pug.compileFile('forgotPassword.pug');
+const compiledFunctionUpdateEmail = pug.compileFile('updateEmail.pug');
 
 module.exports.compiledFunctionEmail = compiledFunctionEmail;
 module.exports.compiledFunctionForgotPassword = compiledFunctionForgotPassword;
+module.exports.compiledFunctionUpdateEmail = compiledFunctionUpdateEmail;
--- routes/updateUser.js
+++ routes/updateUser.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
 const User = require("../model/User");
 const jwt = require("jsonwebtoken");
-const {Bcrypt} = require("bcrypt-rust-wasm");
-const bcrypt = Bcrypt.new(parseInt(process.env.SALT_ROUNDS));
 const {updateValidation} = require("../validation");
+const sgMail = require('@sendgrid/mail');
+const Verified = require('../model/Verified');
+const cryptoRandomString = require('crypto-random-string');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const {compiledFunctionUpdateEmail} = require("../compiledPug");
 
 router.post("/", async (req, res) => {
     const {error} = updateValidation(req.body);
@@ -20,6 +23,32 @@ router.post("/", async (req, res) => {
         const exists = await User.findOne({where: {id: decoded._id}});
         if (exists === null) {
             return res.status(404).json({Error: "User doesnt exist"});
+        }
+        if(email !== exists.email){
+            const token =  cryptoRandomString({length:200, type:'url-safe'});
+            await Verified.create({
+                email: email,
+                token: token
+            });
+            const text = "https://" + req.hostname + "/api/user/verifyEmail/" + token;
+            const emailTemplate = compiledFunctionUpdateEmail({
+                name: name,
+                link: text
+            });
+            const msg = {
+                to: email,
+                from: 'dscvitvellore@gmail.com',
+                subject: 'Email Verification',
+                html: emailTemplate
+            };
+            await sgMail.send(msg);
+            await User.update({
+                name: name,
+                email: email,
+                phone: phone,
+                verified: false
+            }, {where: {id: decoded._id}});
+            return res.json({"Status": "Updated and email sent successfully"});
         }
         await User.update({
             name: name,
