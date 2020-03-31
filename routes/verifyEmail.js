08fb6fd84ee965de7e0e1ac36a7bab205d83b259
mdhishaamakhtar <hishaamakhtar2001.mha@gmail.com>
2020-04-01 02:19:45 +0530

    Update

--- routes/verifyEmail.js
+++ routes/verifyEmail.js
@@ -6,14 +6,14 @@ router.get("/:token",async(req,res)=>{
     const token = req.params.token;
     const data = await Verified.findOne({where:{token}});
     if(data===null){
-        return res.status(400).json({"Error":"Wrong Link"});
+        return res.status(400).render('notverified.ejs');
     }
     const exists = await User.findOne({where:{email:data.email}});
     if(exists===null){
-        return res.status(404).json({"Error":"Email not found"});
+        return res.status(404).render('notverified.ejs');
     }
     await User.update({verified:true},{where:{email:data.email}});
-    res.json({"Message":"Email Verified"});
+    res.render('verified.ejs');
 });
 
 module.exports = router;
