2c04951863c59391cfa284bfefd8c2e0d6753a31
mdhishaamakhtar <hishaamakhtar2001.mha@gmail.com>
2020-04-01 12:57:37 +0530

    Update

--- compiledPug.js
+++
@@ -1,9 +0,0 @@
-const pug = require("pug");
-
-const compiledFunctionEmail = pug.compileFile('email.pug');
-const compiledFunctionForgotPassword = pug.compileFile('forgotPassword.pug');
-const compiledFunctionUpdateEmail = pug.compileFile('updateEmail.pug');
-
-module.exports.compiledFunctionEmail = compiledFunctionEmail;
-module.exports.compiledFunctionForgotPassword = compiledFunctionForgotPassword;
-module.exports.compiledFunctionUpdateEmail = compiledFunctionUpdateEmail;
