044c20ae45b7f7339bff96cffff83d0699489df6
mdhishaamakhtar <hishaamakhtar2001.mha@gmail.com>
2020-04-24 21:50:30 +0530

    Update

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,7 @@ const Sequelize = require('sequelize');
 const dotEnv = require('dotenv');
 dotEnv.config();
 const sequelize = new Sequelize(process.env.DATABASE_URL_NODE, {
-    dialect: 'postgres',
-    pool: {
-        max: 1,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
+    dialect: 'postgres'
 });
 
 module.exports = sequelize;
