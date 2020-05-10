e1cddcf3faf01e3870c9ee989cf2faa2f38f5391
ATechnoHazard <amolele@gmail.com>
2020-05-10 12:46:53 +0530

    Fix connections to postgres
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 const dotEnv = require('dotenv');
 dotEnv.config();
 const sequelize = new Sequelize(process.env.DATABASE_URL_NODE, {
-    dialect: 'postgresql'
+    dialect: 'postgres'
 });
 
 module.exports = sequelize;
