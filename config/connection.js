4b52ac71bd33f68eb61eabe34c15692f59a4424f
ATechnoHazard <amolele@gmail.com>
2020-05-10 13:31:47 +0530

    add debug statements to db conn
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,14 @@
 const Sequelize = require('sequelize');
+console.log("Sequelize imported");
 const dotEnv = require('dotenv');
+console.log("dotenv imported");
 dotEnv.config();
+console.log("dotenv configured");
 const sequelize = new Sequelize(process.env.DATABASE_URL_NODE, {
     dialect: 'postgres'
 });
+console.log("Attempted db conn");
 
 module.exports = sequelize;
+
+console.log("Module exported");
