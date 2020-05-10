60a731f92761b774ea89a78c5f51f23d664ebd66
mdhishaamakhtar <hishaamakhtar2001.mha@gmail.com>
2020-05-10 15:42:27 +0530

    Update

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,9 @@
 const Sequelize = require('sequelize');
-console.log("Sequelize imported");
 const dotEnv = require('dotenv');
-console.log("dotenv imported");
 dotEnv.config();
-console.log("dotenv configured");
 const sequelize = new Sequelize(process.env.DATABASE_URL_NODE, {
     dialect: 'postgres'
 });
-console.log("Attempted db conn");
 
 module.exports = sequelize;
 
